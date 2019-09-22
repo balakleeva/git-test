b07ee19167356304342d920500c997908afb132c
Maria Balakleeva <m.balakleeva@exceed-team.com>
2019-09-22 20:14:32 +0300

    Forgotten changes

--- src/components/Test/index.js
+++ src/components/Test/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Test = () =>{
     return(
         <div>Test
-        <div>Exceed the best!</div>
+            <div>Exceed the best!</div>
+            <div>It's true!</div>
+            <div>Forgotten line</div>
         </div>
     )
 }
